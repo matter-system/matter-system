10e0ed5521a67839d59f1d452e9449f855e6e7c3
simonswiss <simon@simonswiss.com>
2020-05-16 21:55:22 +1000

    syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.setTemplateFormats(["njk", "html", "md", "css"]);
-  eleventyConfig.addPassthroughCopy("../images/navigation/*.svg");
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
